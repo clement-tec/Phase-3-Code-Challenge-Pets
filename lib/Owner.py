bd2f8a4aeae403f031fc1c00d07f8fc9ad4678af
clement senou <clementsenou74@gmail.com>
2023-03-31 13:49:23 -0500

    add somethingg

--- lib/Owner.py
+++ lib/Owner.py
@@ -1,37 +1,39 @@
 from Purchase import Purchase
+
 class Owner:
     def __init__(self, name):
         self._name = name
-        self._purchase = []
+        self._purchases = []
 
     def name(self):
         return self._name
 
-    def get_purchase(self):
-        return self._purchase
+    def get_purchases(self):
+        return self._purchases
 
-    def set_purchase(self, purchase):
-        self._purchase.append(purchase)
+    def set_purchases(self, purchase):
+        self._purchases.append(purchase)
 
-    purchase = property(get_purchase, set_purchase)
+    purchases = property(get_purchases, set_purchases)
 
     def get_animals(self):
         animals_set = set()
-        for purchase in self._purchase:
+        for purchase in self._purchases:
             animals_set.add(purchase.animal)
         return list(animals_set)
+
     animals = property(get_animals)
 
     def purchase_animal(self, animal, price, animal_name):
         purchase = Purchase(animal, price, animal_name)
         purchase.owner = self
-        self.purchase = purchase
+        self.purchases = purchase
 
     def all_purchases(self):
-        return self._purchase
+        return self._purchases
 
     def total_purchases(self):
         total = 0
-        for purchase in self._purchase:
+        for purchase in self._purchases:
             total += purchase.price
         return total
